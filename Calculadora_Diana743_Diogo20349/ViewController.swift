efd863c38f43c1489f6e56c5d4065618e03e5e93
ISTEC <istec@iMac2.local>
2021-01-22 05:14:05 +0000

    new layout

--- Calculadora_Diana743_Diogo20349/ViewController.swift
+++ Calculadora_Diana743_Diogo20349/ViewController.swift
@@ -2,14 +2,53 @@
 //  ViewController.swift
 //  Calculadora_Diana743_Diogo20349
 //
-//  Created by ISTEC on 18/01/2021.
+//  Created by ISTEC on 22/01/2021.
 //  Copyright © 2021 Diana. All rights reserved.
 //
-import UIKit
-import AVFoundation
 
-class ViewController: UIViewController, AVAudioPlayerDelegate {
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayResult: UILabel!
+    
+    var previousNum : Double = 0
+    var currentNumber : Double = 0
+    var preTag = "+"
+    let tagList = ["+","-","*","/"]
+    var modOccured = false
+    var decimal : Bool = false
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
     
 
+    @IBAction func InputClick(_ sender: UIButton) {
+        print(sender.tag)
+                      if (displayResult.text! == "0" || displayResult.text! == "+" || displayResult.text! == "-" || displayResult.text! == "*" || displayResult.text! == "/" || modOccured ) && !(sender.tag == 0) && !(sender.tag==100){
+                          displayResult.text = String(sender.tag)
+                      }
+                      else if sender.tag == 100 && !decimal{
+                          decimal = true
+                          displayResult.text = displayResult.text! + "."
+                      }
+                      else if !(displayResult.text! == "0") && !(sender.tag == 100){
+                          displayResult.text = displayResult.text! + String(sender.tag)
+                      }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
--- Calculadora_Diana743_Diogo20349/ViewControllers/ViewController.swift
+++ Calculadora_Diana743_Diogo20349/ViewControllers/ViewController.swift
@@ -12,110 +12,15 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-    // Ligaçao c/ a label que mostra o resultado
-    @IBOutlet weak var resultDisplay: UILabel!
-    
- 
-    // Declaraçao/Inicializaçao de variáveis
-    let operationsList = ["+","-","*","/"]
+
+    @IBOutlet weak var labelDisplay: UILabel!
+
     var previousNum : Double = 0
-    var actualNum : Double = 0
+    var currentNumber : Double = 0
     var preTag = "+"
-       
-    
+    let tagList = ["+","-","*","/"]
     var modOccured = false
     var decimal : Bool = false
-    
-    // Variáveis para o som do click
-    var audioPlayer : AVAudioPlayer!
-    let clickSound = Bundle.main.url(forResource: "key_sound", withExtension: "mp3")
-    
-    
-    // Função para limpar o resultado
-    @IBAction func ClearDisplay(_ sender: UIButton) {
-        // Play efeito sonoro
-         playKeyPressSoundEffect()
-        
-        
-        // Inicializar todas as variáveis
-        resultDisplay.text="0"
-               previousNum = 0
-               actualNum = 0
-               preTag = "+"
-               modOccured = false
-               decimal = false
-        
-    }
-    
-    
-    // Calcular e mostrar o resultado
-    @IBAction func Calculate(_ sender: UIButton) {
-        
-        // Play efeito sonoro
-               playKeyPressSoundEffect()
-        
-        
-        if previousNum==0 {
-                   previousNum = Double(resultDisplay.text!) ?? 0
-                   decimal = false
-               }
-               else{
-                  
-                   actualNum = Double(resultDisplay.text!) ?? 0
-                   if preTag == "+"{
-                       previousNum += actualNum
-                   }
-                   else if preTag == "-"{
-                       previousNum -= actualNum
-                   }
-                   else if preTag == "*"{
-                       previousNum *= actualNum
-                   }
-                   else if preTag == "/"{
-                       previousNum /= actualNum
-                   }
-               }
-               
-               if sender.tag == 4{
-                   actualNum = Double(resultDisplay.text!) ?? 0
-                   previousNum = actualNum/100
-                   modOccured = true
-                   decimal = true
-               }
-               
-               if sender.tag == 10 || sender.tag == 4{
-                   decimal = true
-                   resultDisplay.text = String(previousNum)
-                   previousNum = 0
-                   preTag = "+"
-               }
-               else{
-                   resultDisplay.text = String(operationsList[sender.tag])
-                   preTag = operationsList[sender.tag]
-               }
-    }
-    
-    
-    
-    
-    // Números
-    @IBAction func ClickNumbers(_ sender: UIButton) {
-        
-         // Play efeito sonoro
-                playKeyPressSoundEffect()
-        
-        
-               if (resultDisplay.text! == "+" || resultDisplay.text! == "-" || resultDisplay.text! == "*" || resultDisplay.text! == "/" || modOccured ) && !(sender.tag == 0) && !(sender.tag==100){
-                   resultDisplay.text = String(sender.tag)
-               }
-               else if sender.tag == 100 && !decimal{
-                   decimal = true
-                   resultDisplay.text = resultDisplay.text! + "."
-               }
-               else if !(resultDisplay.text! == "0") && !(sender.tag == 100){
-                   resultDisplay.text = resultDisplay.text! + String(sender.tag)
-               }    }
-    
     
     
     
@@ -125,12 +30,21 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         // Do any additional setup after loading the view.
     }
     
-    // Funçao encarregue do efeito sonoro
-     func playKeyPressSoundEffect(){
-           audioPlayer = try? AVAudioPlayer(contentsOf: clickSound!)
-           audioPlayer.play()
-           
-       }
+    
+    @IBAction func ClickFunction(_ sender: UIButton) {
+        print(sender.tag)
+        if (labelDisplay.text! == "0" || labelDisplay.text! == "+" || labelDisplay.text! == "-" || labelDisplay.text! == "*" || labelDisplay.text! == "/" || modOccured ) && !(sender.tag == 0) && !(sender.tag==100){
+            labelDisplay.text = String(sender.tag)
+        }
+        else if sender.tag == 100 && !decimal{
+            decimal = true
+            labelDisplay.text = labelDisplay.text! + "."
+        }
+        else if !(labelDisplay.text! == "0") && !(sender.tag == 100){
+            labelDisplay.text = labelDisplay.text! + String(sender.tag)
+        }
+    }
+
     
     
     
